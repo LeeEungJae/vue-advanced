a30aa32e1e731a08b2bc08440df8e28d6d2a59f1
eungjae <eungjlee@smilegate.com>
2021-07-18 17:16:55 +0900

    feat: chart.js 라이브러리 실습

---
+++ VUE-CHARTS/chart-lib/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Chart.js</h1>
+    <bar-chart />
+    <line-chart />
+  </div>
+</template>
+
+<script>
+import BarChart from "./components/BarChart.vue";
+import LineChart from "./components/LineChart.vue";
+export default {
+  name: "App",
+  components: {
+    BarChart,
+    LineChart,
+  },
+};
+</script>
+
+<style></style>
